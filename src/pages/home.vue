3ffd3ad549ab05479f78567ca08fa467efa00a78
zherebtsova-dd <zherebtsova-dd@ranepa.ru>
2022-08-03 22:04:25 +0300

    add vuex

--- src/pages/home.vue
+++ src/pages/home.vue
@@ -1,60 +1,14 @@
 <template>
-   <Form @onSubmit="handleSubmit"/>
-   <List @onRemove="handleRemove" :items="notes" />
+   <p>{{getUsers}}</p>
 </template>
 
 <script>
-import Form from '@/components/Notes/Form.vue';
-import List from '@/components/Notes/List.vue';
 
 export default {
-  components: {
-    Form,
-    List
-  },
-  data() {
-    return {
-      notes: [
-        {
-          title: 'Learn Vue 3',
-          tags: ['work']
-        },
-        {
-          title: 'Finish course',
-          tags: ['work', 'home']
-        },
-        {
-          title: 'Feed cat',
-          tags: []
-        }
-      ],
+  computed: {
+    getUsers() {
+      return this.$store.getters.getUsers;
     }
-  },
-  mounted() {
-    this.getNotes();
-  },
-  watch: {
-    notes: {
-      handler(updatedList) {
-        localStorage.setItem("notes", JSON.stringify(updatedList));
-      },
-      deep: true,
-    },
-  },
-  methods: {
-    getNotes() {
-      const localNotes = localStorage.getItem("notes");
-      if (localNotes) {
-        this.notes = JSON.parse(localNotes);
-      }
-    },
-    handleSubmit(note) {
-      this.notes.push(note);
-    },
-    handleRemove(index) {
-      console.log(`note: ${index} has been removed`);
-      this.notes.splice(index, 1);
-    }
-  },
+  }
 }
 </script>
